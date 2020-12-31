6aa167b8a3060e3fa1b0878000a3b211c2fd7d3c
Rabi Siddique <60459172+Rabi27@users.noreply.github.com>
2020-12-31 10:30:51 +0500

    Implementation of Iterator Design Pattern

---
+++ IteratorDesignPattern/src/iteratordesignpattern/Container.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iteratordesignpattern;
+
+/**
+ *
+ * @author Rabi Siddique
+ */
+public interface Container {
+   public Iterator getIterator();
+}
